d318fefb3b94e94b59564ea9542a456ef42e774e
Robertngatia <ngatiah.robert@gmail.com>
2025-05-23 15:50:54 +0300

    Create python_introduction directory and basic_operation.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+# Basic_operations.py
+
+# Assign values to the two numbers
+number1 = 10
+number2 = 5
+
+# Perform addition
+sum_result = number1 + number2
+print(f"Addition of {number1} and {number2} is {sum_result}")
+
+# Perform subtraction
+difference_result = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+
+# Perform multiplication
+product_result = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {product_result}")
