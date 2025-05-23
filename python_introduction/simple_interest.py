10fb9b42cded9f1d1ba4189c4cbf61a5d6a0e4e0
Robertngatia <ngatiah.robert@gmail.com>
2025-05-23 16:16:48 +0300

    ADD simple_interest.py file into python_introduction directory

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# Simple interest calculation
+# Assigning variables 
+principal = 1000
+rate = 0.05 
+time = 3 
+
+# calculation of simple interest using (I = P * R * T) formula
+
+interest = principal * rate * time 
+print(f"The simple interest is: {interest}")
+
